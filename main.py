058b926cb329ef3a25320b9d5e7d62fa33033904
melserngl <nglmercer@gmail.com>
2024-05-01 00:57:35 -0500

    TTS speechchat
    Implementacion de text to Speech para poder escuchar los mensajes que se reciben

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from TikTokLive import TikTokLiveClient
 from TikTokLive.events import ConnectEvent, CommentEvent
 import threading
 import pyttsx3
+from gtts import gTTS
+from pygame import mixer
+import random
+import os, tempfile, gtts, subprocess, gtts
+import time
 # Create the TikTokLive client instance
 client: TikTokLiveClient = None
+
 def get_available_voices():
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
@@ -19,58 +25,66 @@ def get_available_voices():
     #print(f" - Name: {voice.name}, ID: {voice.id}, Languages: {voice.languages}")
     return voice_names
 
+def hablar(mensaje,lang1):
+    # Usar libreria gTTS
+    volume = 0.7
+    tts = gTTS(mensaje, lang="es" if lang1 is None else lang1, slow=False)
+    ran = random.randint(0,9999)
+    filename = 'Temp' + format(ran) + '.mp3'
+    tts.save(filename)
+    mixer.init()
+    mixer.music.load(filename)
+    mixer.music.set_volume(volume)
+    mixer.music.play()
 
+    while mixer.music.get_busy():
+        time.sleep(0.3)
+
+    mixer.quit()
+    os.remove(filename)
+
+# Imprimir los idiomas soportados
+print("Idiomas soportados por gTTS:")
+LANGUAGES = [language for language in gtts.lang.tts_langs()]
+print(LANGUAGES)
 def main(page):
     title = ft.Text("Available Text-to-Speech Voices")
-    def init_text_to_speech():
-        engine = pyttsx3.init()
-        return engine
-
-# Función para leer el texto
-    def speak(engine, text):
-        engine.say(text)
-        engine.runAndWait()
-
-    # Función que se ejecuta cuando se cambia el valor del Dropdown
-    def dropdown_changed(e):
-        texto.value = f"Dropdown changed to {voice_dropdown.value}"
-        # Leer el texto seleccionado en el Dropdown
-        speak(voice_dropdown.value, voice_dropdown.value)
-        page.update()
+    uniqueId = page.client_storage.get("uniqueId")
     # Fetch voice data from the function
     voice_names = get_available_voices()
     def dropdown_changed(e):
         texto.value = f"Dropdown changed to {voice_dropdown.value}"
-        
+        hablar(voice_dropdown.value,voice_dropdown.value)
         page.update()
+    
     # Create dropdown options from the voice names
     voice_dropdown = ft.Dropdown(on_change=dropdown_changed,width=300)  # Initialize dropdown
-    
-    for name in voice_names:
-        option = ft.dropdown.Option(key=name, text=name)
+    ft.dropdown.Option(key=uniqueId,text=uniqueId)
+    for lang in LANGUAGES:
+        option = ft.dropdown.Option(key=lang, text=lang)
         voice_dropdown.options.append(option)  # Add option to dropdown
     # Create the dropdown control
-
+    
     # Add elements to the page
     page.add(title)
     page.add(voice_dropdown)
     texto = ft.Text("TiktokLive")
     chat = ft.Column(scroll="auto", height=400)  # Se agrega scroll al chat
-    unique_id_input = ft.TextField(label="Escribe UniqueId")
+    unique_id_input = ft.TextField(label="Escribe UniqueId" ,value=uniqueId)
 
     def on_connect(event: ConnectEvent):
         print(f"Connected to @{event.unique_id} (Room ID: {client.room_id})")
-
+    
     async def on_comment(event: CommentEvent):
         print(f"{event.user.nickname} -> {event.comment}")
+        hablar(event.comment,voice_dropdown.value)
         chat.controls.append(ft.Text(f"{event.user.nickname} -> {event.comment}"))
         page.update()
-
     def connect_tiktok_live():
         global client
         if not client:
             client = TikTokLiveClient(unique_id=unique_id_input.value)
-
+            page.client_storage.set("uniqueId", unique_id_input.value)
             @client.on(ConnectEvent)
             async def on_connect_wrapper(event: ConnectEvent):
                 on_connect(event)
@@ -95,7 +109,7 @@ def main(page):
         page.update()
 
     page.pubsub.subscribe(enviar_mensaje_tunel)
-
+    
     def enviar_mensaje(evento):
         page.pubsub.send_all({"texto": campo_mensaje.value, "usuario": unique_id_input.value,
                                 "tipo": "mensaje"})
